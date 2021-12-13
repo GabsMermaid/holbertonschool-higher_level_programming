30c204d9e72854d6f2b4e78ceade7bcdbb8cb2fc
Gabriela <g.martinez.rodz@gmail.com>
2021-12-13 19:19:47 +0000

    Testing correction

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const arg in args) {
-  console.log(args[arg]);
+const length = args.length;
+let i = 0;
+for (i = 0; i < length; i++) {
+  console.log(args[i]);
 }
