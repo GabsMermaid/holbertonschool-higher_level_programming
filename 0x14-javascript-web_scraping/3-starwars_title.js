e39dea87b11bf3bec0f8c33a06be1d00307d69b2
GabsMermaid <g_martinez_rodz@gmail.com>
2022-02-02 02:42:11 +0000

    Task 3/SEMISTANDARD FIXED

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,6 +6,6 @@ request(url + process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-	  console.log(JSON.parse(body).title);
+    console.log(JSON.parse(body).title);
   }
 });
