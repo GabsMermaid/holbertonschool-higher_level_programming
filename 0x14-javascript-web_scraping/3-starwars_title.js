91c5807114e679d868bc450c87382180e1e68035
GabsMermaid <g_martinez_rodz@gmail.com>
2022-02-02 02:39:07 +0000

    Task 3/Testing with expected output

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
-request(url, function (error, response, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url + process.argv[2], function (error, response, body) {
   if (error) {
-    console.error(error);
+    console.log(error);
+  } else {
+	  console.log(JSON.parse(body).title);
   }
-  console.log(JSON.parse(body).title);
 });
